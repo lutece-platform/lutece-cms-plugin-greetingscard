24ec86618efad368a1d8d44de161778c38acfb7e
Emeric <emeric@ejudith.fr>
2023-10-31 11:52:16 +0100

    LUT-26648 - Fixing template not loading in FO and permissions in BO

---
+++ src/sql/plugins/greetingscard/upgrade/update_db_core_greetingscard-3.0.0-3.0.1.sql
@@ -0,0 +1,13 @@
+INSERT INTO core_admin_role_resource (role_key, resource_type, resource_id, permission) VALUES ('all_site_manager', 'GREETINGS_CARD_TYPE','*','CREATE_CARD');
+
+INSERT INTO core_admin_role_resource (role_key, resource_type, resource_id, permission) VALUES ('all_site_manager', 'GREETINGS_CARD_TYPE','*','SEND_CARD');
+
+INSERT INTO core_admin_role_resource (role_key, resource_type, resource_id, permission) VALUES ('all_site_manager', 'GREETINGS_CARD_TYPE','*','MODIFY_CARD');
+
+INSERT INTO core_admin_role_resource (role_key, resource_type, resource_id, permission) VALUES ('all_site_manager', 'GREETINGS_CARD_TYPE','*','DELETE_CARD');
+
+INSERT INTO core_admin_role_resource (role_key, resource_type, resource_id, permission) VALUES ('all_site_manager', 'GREETINGS_CARD_TYPE','*','SEE_STATS');
+
+INSERT INTO core_admin_role_resource (role_key, resource_type, resource_id, permission) VALUES ('all_site_manager', 'GREETINGS_CARD_TYPE','*','EXPORT');
+
+INSERT INTO core_admin_role_resource (role_key, resource_type, resource_id, permission) VALUES ('all_site_manager', 'GREETINGS_CARD_TYPE','*','ARCHIVE');
